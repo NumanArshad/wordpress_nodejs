0eb9ea8a2f732720c2578a09986eefebcbf71c8d
Numan Arshad <numanuet311@gmail.com>
2020-05-02 01:04:48 +0500

    react app with webpack config

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class App extends React.Component{
+    constructor(props){
+super(props)
+    }
+
+    render(){
+        return <h1>welcme sjbds o webpack app</h1>
+    }
+    
+}
+export  default App
